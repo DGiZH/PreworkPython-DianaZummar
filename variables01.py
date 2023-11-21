542ba43c6c928299ad508fda5cab087383c9cb5d
DGiZH <zummardiana@gmail.com>
2023-11-21 20:36:12 +0100

    DataAnalytics_Prework_variables01

---
+++ variables01.py
@@ -0,0 +1,29 @@
+'''1. Ejercicio: Crea una variable llamada nombre y asígnale tu nombre como valor.
+Luego, imprime la variable.
+2. Ejercicio: Crea dos variables, a y b , y asígnales los valores 5 y 10
+respectivamente. Luego, imprime la suma de a y b .
+3. Ejercicio: Calcula el área de un triángulo con base 10 y altura 5.
+4. Ejercicio: Calcula el resto de dividir 17 entre 3.'''
+
+#1. Ejercicio
+nombre = 'Diana'
+print(nombre)
+
+# 2. Ejercicio
+a = 5
+b = 10
+print(a+b)
+
+# 3. Ejercicio
+area_triangle = a * b / 2
+print(area_triangle)
+
+#4. Ejercicio
+print(17%3)
+
+
+
+  
+
+
+
